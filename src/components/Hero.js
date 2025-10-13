f32b26c996d087b3431cc2c9dc0433e7b80041c1
Ashutosh Pradhan <124667480+helloashutoshp@users.noreply.github.com>
2025-10-13 21:18:10 +0530

    image updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Hero.css';
+import profileImage from '../assets/Profile.jpg';
 
 const Hero = () => {
   return (
@@ -8,7 +9,7 @@ const Hero = () => {
         <div className="hero-image-section">
           <div className="profile-image-wrapper">
             <img
-              src="/Images/Profile.jpg"
+              src={profileImage}
               alt="Ashutosh Pradhan"
               className="profile-image"
             />
